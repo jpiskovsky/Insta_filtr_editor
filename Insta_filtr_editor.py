70d92d8aa7fca1bc0be6c685a4f83018e7184bee
Pískovský <pis41264@spseol.cz>
2024-03-26 21:09:57 +0100

    syty_filtr

--- Insta_filtr_editor.py
+++ Insta_filtr_editor.py
@@ -29,3 +29,20 @@ while x < sirka:
         y += 1
     x += 1
 obrazek.show()
+
+#3
+obrazek = Image.open("blesk.jpg")
+sirka, vyska = obrazek.size
+x = 0
+while x < sirka:
+    y = 0
+    while y < vyska:
+        r, g, b = obrazek.getpixel((x,y))
+        prumer = int((r+g+b)/3)
+        if prumer > 150:
+            obrazek.putpixel((x,y), (r*2, g*2, b*2))  
+        else:
+            obrazek.putpixel((x,y), (r//2, g//2, b//2))  
+        y += 1
+    x += 1
+obrazek.show()
